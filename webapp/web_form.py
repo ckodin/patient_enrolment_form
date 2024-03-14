3c426c481738bd646ee97da028643d1f579c5beb
clrslook <97978567+clrslook@users.noreply.github.com>
2024-03-14 11:57:38 +0800

    Initial commit

--- webapp/web_form.py
+++ webapp/web_form.py
@@ -5,6 +5,7 @@ import json
 import datetime 
 import requests 
 import urllib3
+from uuid import uuid4
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,18 +16,21 @@ def inject_custom_css():
 inject_custom_css()
 
 
-def form_enrolment_request(identifier_nhi, first_name, last_name, gender, birth_date, phone_mobile, email_personal): 
-        birth_date_str = birth_date.strftime("%Y-%m-%d") if birth_date else None
-        patient = {
-              "identifier_nhi": identifier_nhi, 
-              "first_name" : first_name,
-              "last_name": last_name,
-              "gender": gender, 
-              "birth_date": birth_date_str,
-              "phone_mobile": phone_mobile,
-              "email_personal": email_personal
+def form_enrolment_request(phone_mobile, email_personal, hhs, ward, procedure_type):
+    request_body = {
+        "messageId": uuid4().hex,
+        "patient": {
+            "phone_mobile": phone_mobile,
+            "email_personal": email_personal
+        },
+        "participation": {
+            "hhs": hhs,
+            "ward": ward,
+            "procedure_type": procedure_type
         }
-        return {"patient": patient }
+    }
+    return request_body
+
 
 def send_enrolment_request(body): 
       payload = json.dumps(body)
@@ -42,22 +46,20 @@ def send_enrolment_request(body):
 
 
 def main(): 
-      st.title("Patient Enrolment Form") 
+      st.title("PREM Enrolment Form") 
 
-      with st.form("enrol_form"): 
-            identifier_nhi = st.text_input("National health identifier")
-            first_name = st.text_input("First name")
-            last_name = st.text_input("Last name")
-            gender = st.selectbox("Gender", ["Male", "Female", "Diverse", "Unknown"], index=None)
-            birth_date = st.date_input("Birth date", value=None, format="DD/MM/YYYY")
+      with st.form("enrol_form"):             
             phone_mobile = st.text_input("Mobile phone")
             email_personal = st.text_input("Email address")
+            hhs = st.selectbox("HHS/Hospitals", ["Alpha Private Surgery","Ayr Hospital","Bamaga Hospital","Boonah Health Service","Caboolture Hospital","Charleville Hospital","Coen Primary Health Care Centre","Dysart Hospital","Esk Health Service","Gatton Health Service","Gympie Hospital","Home Hill Hospital", "Inala Community Health Centre","Ipswich Hospital", "Moura Hospital","Nambour General Hospital","The Royal Brisbane and Women's Hospital","Thursday Island Hospital","Warwick Health Service"], index=None)
+            ward = st.text_input("Ward")
+            procedure_type = st.text_input("Procedure Type")
             
             st.write("")
             st.write("")
       
             if st.form_submit_button('Enrol in ZEDOC', type="primary"): 
-                  request_body = form_enrolment_request(identifier_nhi, first_name, last_name, gender, birth_date, phone_mobile, email_personal)
+                  request_body = form_enrolment_request(phone_mobile, email_personal, hhs, ward, procedure_type)
                   response_status = send_enrolment_request(request_body)
                   if response_status == 200: 
                         st.success("Patient successfully enrolled.")
