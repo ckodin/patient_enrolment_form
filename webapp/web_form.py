e9e7dcfb16db3704120bc1159fdb4082ec7eb9b5
clrslook <97978567+clrslook@users.noreply.github.com>
2024-03-17 23:24:38 +0800

    Update variables to new PREM variables

--- webapp/web_form.py
+++ webapp/web_form.py
@@ -16,17 +16,19 @@ def inject_custom_css():
 inject_custom_css()
 
 
-def form_enrolment_request(phone_mobile, email_personal, hhs, ward, procedure_type):
+def form_enrolment_request(first_name, phone_mobile, hospital, ward, unit, hhs, peer_group):
     request_body = {
         "messageId": uuid4().hex,
         "patient": {
-            "phone_mobile": phone_mobile,
-            "email_personal": email_personal
+            "first_name": first_name,
+            "phone_mobile": phone_mobile
         },
         "participation": {
-            "hhs": hhs,
+            "hospital": hospital,
             "ward": ward,
-            "procedure_type": procedure_type
+            "unit": unit,
+            "hhs": hhs,
+            "peer_group": peer_group
         }
     }
     return request_body
@@ -48,23 +50,32 @@ def send_enrolment_request(body):
 def main(): 
       st.title("PREM Enrolment Form") 
 
-      with st.form("enrol_form"):             
+      with st.form("enrol_form"):   
+            first_name = st.text_input("First name")          
             phone_mobile = st.text_input("Mobile phone")
-            email_personal = st.text_input("Email address")
-            hhs = st.selectbox("HHS/Hospitals", ["Alpha Private Surgery","Ayr Hospital","Bamaga Hospital","Boonah Health Service","Caboolture Hospital","Charleville Hospital","Coen Primary Health Care Centre","Dysart Hospital","Esk Health Service","Gatton Health Service","Gympie Hospital","Home Hill Hospital", "Inala Community Health Centre","Ipswich Hospital", "Moura Hospital","Nambour General Hospital","The Royal Brisbane and Women's Hospital","Thursday Island Hospital","Warwick Health Service"], index=None)
+            hospital = st.selectbox("Hospital", ["Alpha Private Surgery","Ayr Hospital","Bamaga Hospital","Boonah Health Service","Caboolture Hospital","Charleville Hospital","Coen Primary Health Care Centre","Dysart Hospital","Esk Health Service","Gatton Health Service","Gympie Hospital","Home Hill Hospital", "Inala Community Health Centre","Ipswich Hospital", "Moura Hospital","Nambour General Hospital","The Royal Brisbane and Women's Hospital","Thursday Island Hospital","Warwick Health Service"], index=None)
             ward = st.text_input("Ward")
-            procedure_type = st.text_input("Procedure Type")
+            unit = st.text_input("Unit")
+            hhs = st.selectbox("Hospital and health services", ["Cairns and Hinterland", "Central Queensland", "Central West", "Darling Downs", "Gold Coast", "Mackay", "Metro North", "Metro South", "North West", "South West", "Sunshine Coast", "Torres and Cape", "Townsville", "West Moreton", "Wide Bay", "Children's Health Queensland"], index=None)
+            peer_group = st.selectbox("Peer group", ["Small hospital", "Medium hospital", "Large hospital", "Psychiatric hospital", "Day hospital"], index=None)
             
             st.write("")
             st.write("")
       
             if st.form_submit_button('Enrol in ZEDOC', type="primary"): 
-                  request_body = form_enrolment_request(phone_mobile, email_personal, hhs, ward, procedure_type)
-                  response_status = send_enrolment_request(request_body)
-                  if response_status == 200: 
+                try:
+                    print("Enrolment process")
+                    request_body = form_enrolment_request(first_name, phone_mobile, hospital, ward, unit, hhs, peer_group)
+                    print("Request body formed")
+                    response_status = send_enrolment_request(request_body)
+                    print("Enrolment request sent")
+                    if response_status == 200: 
                         st.success("Patient successfully enrolled.")
-                  else: 
+                    else: 
                         st.error("An error occurred.")
+                except Exception as e:
+                    st.error(f"An error occurred: {str(e)}")
+
 
 if __name__ == '__main__':
      main()
